bfb2b1c96886dfc149322de628c1e69d882949df
Kevin Murphy <kevin@eorbit.net>
2024-08-17 10:48:53 -0700

    Add script to swap musketeers for pillagers.

---
+++ kubejs/server_scripts/musketeers_replace_pillagers.js
@@ -0,0 +1,14 @@
+EntityEvents.spawned(event => {
+    // Define constants
+    const entity = event.entity;
+
+    // Skip the logic if the entity is a player or is not living
+    if (entity.isPlayer() || !entity.isLiving()) {
+        return;
+    }
+
+    if (entity.type == "minecraft:pillager") {
+        event.server.runCommandSilent(`execute in ${entity.level.dimension} positioned ${entity.x} ${entity.y} ${entity.z} run summon guntotingillagers:musketeer`)
+        event.cancel()
+    }
+});
