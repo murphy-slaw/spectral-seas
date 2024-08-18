165fc7b57d03fea545a4935ddba5a0487bc097be
Kevin Murphy <kevin@eorbit.net>
2024-08-17 17:37:15 -0700

    Replace Hybrid Aquatic Coconut Crabs with the Ecologics version.

--- kubejs/server_scripts/spawn_replacements.js
+++ kubejs/server_scripts/spawn_replacements.js
@@ -11,4 +11,9 @@ EntityEvents.spawned(event => {
         event.server.runCommandSilent(`execute in ${entity.level.dimension} positioned ${entity.x} ${entity.y} ${entity.z} run summon guntotingillagers:musketeer`)
         event.cancel()
     }
+
+    if (entity.type == "hybrid-aquatic:coconut_crab"){
+        event.server.runCommandSilent(`execute in ${entity.level.dimension} positioned ${entity.x} ${entity.y} ${entity.z} run summon ecologics:coconut_crab`)
+        event.cancel()
+    }
 });
