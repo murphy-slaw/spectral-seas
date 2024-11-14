b169fd47e71fabb71a49876ff874ad636e7b2634
Kevin Murphy <kevin@eorbit.net>
2024-11-13 16:44:53 -0800

    Use scoreboards for chest tracking.

--- kubejs/server_scripts/pity_chest.js
+++ kubejs/server_scripts/pity_chest.js
@@ -1,18 +1,24 @@
-const LOOT_TABLE_KEY = "LootTableSeenCounts"
+let ObjectiveCriteria = Java.loadClass("net.minecraft.world.scores.criteria.ObjectiveCriteria");
 
 /*
     Keep track of how many times a player has opened a chest with the given loot table.
-    We can get the NBT data in the loot tables themselves to regulate rare drops.
+    We can get the score data in the loot tables themselves to regulate rare drops.
 */
 
 BlockEvents.rightClicked('minecraft:chest', event => {
     let lootTable = event.block.getEntityData().getString("LootTable")
     if (lootTable != '') {
-        let player = event.player
-        let counts = player.persistentData.getCompound(LOOT_TABLE_KEY)
-        let count = counts.getInt(lootTable)
-        count = count + 1
-        counts.putInt(lootTable, count)
-        player.persistentData.put(LOOT_TABLE_KEY, counts)
+        let scoreboard = event.level.scoreboard
+        let objectiveName = "loot." + lootTable
+        let objective = scoreboard.getObjective(objectiveName)
+        if (!objective) {
+            objective = scoreboard.addObjective(
+                objectiveName, ObjectiveCriteria.DUMMY,
+                Component.of(objectiveName),
+                ObjectiveCriteria.DUMMY.getDefaultRenderType()
+            );
+        }
+        let score = scoreboard.getOrCreatePlayerScore(event.player.username, objective)
+        score.increment()
     }
 })
