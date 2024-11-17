e170c2047fd8e4942e313bcfd0a47034a6b62282
Kevin Murphy <kevin@eorbit.net>
2024-11-17 04:32:32 -0800

    More turtle tweaks, tweak wild flax placement

--- kubejs/server_scripts/items.js
+++ kubejs/server_scripts/items.js
@@ -1,10 +1,11 @@
 const $FluidTags = Java.loadClass('net.minecraft.tags.FluidTags');
-
 PlayerEvents.tick(event => {
-    let player = event.player;
+    let player = event.entity;
     if (!(player.age % 20 === 0)) return;
-    if (player.getHeadArmorItem().id === 'minecraft:turtle_helmet' && !player.isEyeInFluid($FluidTags.WATER)) {
-        player.potionEffects.add("minecraft:water_breathing", 3600, 0, false, true);
-        player.potionEffects.add("hybrid-aquatic:clarity", 3600, 0, false, true);
+    let helmet = player.getHeadArmorItem();
+    if (helmet.id === 'minecraft:turtle_helmet' && player.isEyeInFluid($FluidTags.WATER)) {
+        player.potionEffects.add("minecraft:water_breathing", 40, 0, false, true);
+        player.potionEffects.add("hybrid-aquatic:clarity", 40, 0, false, true);
+        helmet.hurtAndBreak(1, player, entity => { return true; });
     }
 });
