208b232de19ae1d2ba47ab8c4627525218749dc2
Kevin Murphy <kevin@eorbit.net>
2024-12-31 12:35:00 -0800

    Make it so disembarking a ship doesn't clear your spawn point.

--- kubejs/server_scripts/ship_spawn.js
+++ kubejs/server_scripts/ship_spawn.js
@@ -4,7 +4,6 @@ const shipTypes = [
     "smallships:drakkar",
     "smallships:galley"
 ];
-
 PlayerEvents.tick(event => {
     const player = event.player;
     const vehicle = player.getVehicle();
@@ -12,6 +11,13 @@ PlayerEvents.tick(event => {
     if (vehicle) {
         if (shipTypes.includes(vehicle.type)) {
             if (!player.tags.contains('on_ship')) {
+                let shipID = player.persistentData.get("shipID");
+                let vehicleUuid = vehicle.getUuid().toString();
+                console.info(`Vehicle UUID: ${vehicleUuid}\n shipId: ${shipID}`);
+                if (!shipID || shipID != vehicleUuid) {
+                    console.info("Setting shipID");
+                    player.persistentData.put("shipID", vehicleUuid.toString());
+                }
                 player.addTag("on_ship");
                 let markerPos = player.persistentData.getCompound("MarkerPosition");
                 if (markerPos) {
@@ -28,10 +34,31 @@ PlayerEvents.tick(event => {
             markerPos.z = Math.floor(player.z);
             player.persistentData.put("MarkerPosition", markerPos);
             Utils.server.scheduleInTicks(1, callback => {
-                Utils.server.runCommandSilent(`execute as ${player.username} run spawnpoint @s ${markerPos.x} ${markerPos.y} ${markerPos.z}`);
-                Utils.server.runCommandSilent(`execute as ${player.username} run surveyor landmarks add surveyor:point ${markerPos.x} ${markerPos.y} ${markerPos.z} red Ship`);
+                //Utils.server.runCommandSilent(`execute as ${player.username} run spawnpoint @s ${markerPos.x} ${markerPos.y} ${markerPos.z}`);
+                Utils.server.runCommandSilent(`execute as ${player.username} run surveyor landmarks add surveyor:point ${markerPos.x} ${markerPos.y} ${markerPos.z} red ${player.username}'s Ship`);
             });
-            Utils.server.runCommandSilent(`tellraw ${player.username} "Spawnpoint has been set to your ship\'s location"`);
         }
     }
-});;
+});
+
+EntityEvents.death('minecraft:player', event => {
+    let shipID = event.player.persistentData.get("shipID");
+    if (shipID) {
+        console.info(`Ship ID: ${shipID}`);
+        let ship = null;
+        let ents = event.level.getEntities();
+        for (let ent of ents) {
+            if (ent.uuid.toString() == shipID) {
+                ship = ent;
+                break;
+            }
+        }
+        if (ship) {
+            console.info(`Ship: ${ship}`);
+            Utils.server.runCommand(`execute as ${event.player.username} run spawnpoint @s ${Math.trunc(ship.x)} ${Math.trunc(ship.y) + 1} ${Math.trunc(ship.z)}`);
+        } else {
+            console.info(`Ship not found!`);
+            event.player.persistentData.delete("shipID");
+        }
+    }
+});
