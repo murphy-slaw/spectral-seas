79071ec0d8dfaabd8f82ed49a4803d6c65f30aae
Kevin Murphy <kevin@eorbit.net>
2025-01-27 06:37:37 -0800

    Add Grave Dirt block.

---
+++ kubejs/startup_scripts/grave_dirt.js
@@ -0,0 +1,21 @@
+StartupEvents.registry('block', event => {
+    event.create('grave_dirt')
+        .displayName("Grave Dirt")
+        .hardness(0.5)
+        .resistance(0.5)
+        .requiresTool(true)
+        .tagBlock("minecraft:mineable/shovel")
+        .textureAll("minecraft:block/dirt")
+        .blockEntity(entityInfo => {
+            entityInfo.serverTick(60, 0, entityEvent => global.reloadableParticle(entityEvent));
+        });
+});
+
+global.reloadableParticle = entityEvent => {
+    let pos = entityEvent.blockPos.above();
+    if ((entityEvent.tick % 4 == 0) && entityEvent.level.getBlock(pos) == "minecraft:air") {
+        entityEvent.level.runCommandSilent(
+            `particle soul ${pos.x} ${pos.y + 0.2} ${pos.z} 0.001 0.01 0.001 0.01 1`
+        );
+    }
+};
