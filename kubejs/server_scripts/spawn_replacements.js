b198b3bd7fdfd7df58f2827442c934d48ba9681a
Kevin Murphy <kevin@eorbit.net>
2025-02-01 20:11:30 -0800

    Increase trident drop rates for drowned.

--- kubejs/server_scripts/spawn_replacements.js
+++ kubejs/server_scripts/spawn_replacements.js
@@ -1,22 +1,22 @@
 const scaleTypes = Java.loadClass("virtuoel.pehkui.api.ScaleTypes");
-const baseScale = scaleTypes.BASE
+const baseScale = scaleTypes.BASE;
 
 const pillagerHats = [
     "simplehats:bicorne",
     "simplehats:tricorne",
     "simplehats:eyepatch",
     "minecraft:empty"
-]
+];
 
 const vindicatorWeapons = [
     "simplyswords:iron_cutlass",
     "simplyswords:iron_rapier",
     "minecraft:iron_axe"
-]
+];
 
 const pillagerWeapons = [
     "musketmod:pistol"
-]
+];
 
 EntityEvents.spawned(event => {
     // Define constants
@@ -31,32 +31,36 @@ EntityEvents.spawned(event => {
         entity.setItemSlot(
             "head",
             Item.of(Utils.randomOf(Utils.random, pillagerHats))
-        )
+        );
         entity.setItemSlot(
             "mainhand",
             Item.of(Utils.randomOf(Utils.random, pillagerWeapons))
-        )
+        );
     }
 
     if (entity.type == "minecraft:vindicator") {
         entity.setItemSlot(
             "head",
             Item.of(Utils.randomOf(Utils.random, pillagerHats))
-        )
+        );
         entity.setItemSlot(
             "mainhand",
             Item.of(Utils.randomOf(Utils.random, vindicatorWeapons))
-        )
+        );
     }
 
     if (entity.type == "hybrid-aquatic:coconut_crab") {
         // event.server.runCommandSilent(`execute in ${entity.level.dimension} positioned ${entity.x} ${entity.y} ${entity.z} run summon ecologics:coconut_crab`)
-        event.cancel()
+        event.cancel();
     }
 
     if (entity.type == "hybrid-aquatic:karkinos") {
-        console.infof("Summoned Karkinos: %s", entity.uuid)
-        baseScale.getScaleData(entity).setScale(3)
+        console.infof("Summoned Karkinos: %s", entity.uuid);
+        baseScale.getScaleData(entity).setScale(3);
     }
-
+    if (entity.type == "minecraft:drowned") {
+        var data = entity.nbt;
+        data.put("HandDropChances", NBT.listTag([NBT.floatTag(0.085), NBT.floatTag(0.3)]));
+        entity.setNbt(data);
+    }
 });
