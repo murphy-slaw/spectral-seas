1bf52e21d2d6b33bc278b7fb2faeb324e0bcf12d
Kevin Murphy <kevin@eorbit.net>
2025-02-07 22:51:36 -0800

    Fix textures for ship upgrades.

--- kubejs/startup_scripts/items/ship_upgrades.js
+++ kubejs/startup_scripts/items/ship_upgrades.js
@@ -1,12 +1,12 @@
 StartupEvents.registry('item', event => {
     event.create('ship_speed_upgrade')
         .displayName('Ship Speed Upgrade')
-        .texture('kubejs:item/ship_speed_upgrade')
+        .texture('spectral_seas:item/ship_speed_upgrade')
         .unstackable()
         .useDuration(itemstack => 20);
     event.create('ship_cargo_upgrade')
         .displayName('Ship Cargo Upgrade')
-        .texture('kubejs:item/ship_storage_upgrade')
+        .texture('spectral_seas:item/ship_storage_upgrade')
         .unstackable()
         .useDuration(itemstack => 20);
 });
