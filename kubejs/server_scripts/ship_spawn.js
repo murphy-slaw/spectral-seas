e8fb8871055c7641f4fbb48d0a57686aa976239f
Kevin Murphy <kevin@eorbit.net>
2025-02-07 22:50:06 -0800

    Ship spawn marker tweaks.

--- kubejs/server_scripts/ship_spawn.js
+++ kubejs/server_scripts/ship_spawn.js
@@ -7,16 +7,16 @@ const shipTypes = [
 PlayerEvents.tick(event => {
     const player = event.player;
     const vehicle = player.getVehicle();
+    const shipID = player.persistentData.getString("shipID");
 
     if (vehicle) {
         if (shipTypes.includes(vehicle.type)) {
             if (!player.tags.contains('on_ship')) {
-                let shipID = player.persistentData.get("shipID");
                 let vehicleUuid = vehicle.getUuid().toString();
                 console.info(`Vehicle UUID: ${vehicleUuid}\n shipId: ${shipID}`);
                 if (!shipID || shipID != vehicleUuid) {
-                    console.info("Setting shipID");
-                    player.persistentData.put("shipID", vehicleUuid.toString());
+                    console.info(`Setting shipID: ${vehicleUuid}`);
+                    player.persistentData.putString("shipID", vehicleUuid);
                 }
                 player.addTag("on_ship");
                 let markerPos = player.persistentData.getCompound("MarkerPosition");
@@ -28,14 +28,17 @@ PlayerEvents.tick(event => {
     } else {
         if (player.tags.contains("on_ship")) {
             player.removeTag("on_ship");
+            let ship = event.level.getEntity(UUID.fromString(shipID));
             let markerPos = {};
             markerPos.x = Math.floor(player.x);
             markerPos.y = Math.floor(player.y);
             markerPos.z = Math.floor(player.z);
             player.persistentData.put("MarkerPosition", markerPos);
+            console.log(`Marker position: ${markerPos}`);
+            let color = ship.nbt.get("Sail").getString("Color");
+            let type = ship.nbt.getString("Type");
             Utils.server.scheduleInTicks(1, callback => {
-                //Utils.server.runCommandSilent(`execute as ${player.username} run spawnpoint @s ${markerPos.x} ${markerPos.y} ${markerPos.z}`);
-                Utils.server.runCommandSilent(`execute as ${player.username} run surveyor landmarks add surveyor:point ${markerPos.x} ${markerPos.y} ${markerPos.z} red ${player.username}'s Ship`);
+                Utils.server.runCommandSilent(`execute as ${player.username} run surveyor landmarks add surveyor:point ${markerPos.x} ${markerPos.y} ${markerPos.z} ${color} ${player.username}'s ${type} ${ship.name.string.toLocaleLowerCase()}`);
             });
         }
     }
@@ -45,14 +48,7 @@ EntityEvents.death('minecraft:player', event => {
     let shipID = event.player.persistentData.get("shipID");
     if (shipID) {
         console.info(`Ship ID: ${shipID}`);
-        let ship = null;
-        let ents = event.level.getEntities();
-        for (let ent of ents) {
-            if (ent.uuid.toString() == shipID) {
-                ship = ent;
-                break;
-            }
-        }
+        let ship = event.level.getEntity(UUID.fromString(shipID))
         if (ship) {
             console.info(`Ship: ${ship}`);
             Utils.server.runCommand(`execute as ${event.player.username} run spawnpoint @s ${Math.trunc(ship.x)} ${Math.trunc(ship.y) + 1} ${Math.trunc(ship.z)}`);
