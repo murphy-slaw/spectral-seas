5e6b133a42d4c6cc279cf5f6ba65a9bd580ba6da
Kevin Murphy <kevin@eorbit.net>
2025-02-21 19:26:38 -0800

    More pillager hats. Tweak drowned armor.

--- kubejs/server_scripts/spawn_replacements.js
+++ kubejs/server_scripts/spawn_replacements.js
@@ -1,10 +1,15 @@
 const baseScale = scaleTypes.BASE
+const MobType = Java.loadClass('net.minecraft.world.entity.MobType')
 
 const pillagerHats = [
-    'simplehats:bicorne',
-    'simplehats:tricorne',
-    'simplehats:eyepatch',
-    'minecraft:empty',
+    Item.of('simplehats:bicorne'),
+    Item.of('simplehats:eyepatch'),
+    Item.of('simplehats:ushanka'),
+    //'simplehats:dorkglassesandteeth',
+    //'simplehats:bowler',
+    Item.of('minecraft:iron_helmet').withNBT({ style: 'pirate' }),
+    Item.of('minecraft:chainmail_helmet').withNBT({ style: 'pirate' }),
+    Item.of('minecraft:leather_helmet').withNBT({ style: 'pirate' }),
 ]
 
 const vindicatorWeapons = [
@@ -24,26 +29,23 @@ EntityEvents.spawned(event => {
         return
     }
 
-    if (entity.type == 'minecraft:pillager') {
-        entity.setItemSlot(
-            'head',
-            Item.of(Utils.randomOf(Utils.random, pillagerHats))
-        )
-        entity.setItemSlot(
-            'mainhand',
-            Item.of(Utils.randomOf(Utils.random, pillagerWeapons))
-        )
-    }
-
-    if (entity.type == 'minecraft:vindicator') {
-        entity.setItemSlot(
-            'head',
-            Item.of(Utils.randomOf(Utils.random, pillagerHats))
-        )
-        entity.setItemSlot(
-            'mainhand',
-            Item.of(Utils.randomOf(Utils.random, vindicatorWeapons))
-        )
+    if (entity.mobType === MobType.ILLAGER) {
+        //Don't replace raid captain banner
+        if (entity.getHeadArmorItem().empty) {
+            entity.setHeadArmorItem(Utils.randomOf(Utils.random, pillagerHats))
+        }
+        if (entity.type == 'minecraft:pillager') {
+            entity.setItemSlot(
+                'mainhand',
+                Item.of(Utils.randomOf(Utils.random, pillagerWeapons))
+            )
+        }
+        if (entity.type == 'minecraft:vindicator') {
+            entity.setItemSlot(
+                'mainhand',
+                Item.of(Utils.randomOf(Utils.random, vindicatorWeapons))
+            )
+        }
     }
 
     if (entity.type == 'hybrid-aquatic:coconut_crab') {
@@ -55,12 +57,30 @@ EntityEvents.spawned(event => {
         console.infof('Summoned Karkinos: %s', entity.uuid)
         baseScale.getScaleData(entity).setScale(3)
     }
-    if (entity.type == 'minecraft:drowned') {
+    if (entity.type === 'minecraft:drowned') {
         var data = entity.nbt
         data.put(
             'HandDropChances',
             NBT.listTag([NBT.floatTag(0.085), NBT.floatTag(0.3)])
         )
         entity.setNbt(data)
+    }
+
+    if (entity.type === 'guardvillagers:guard') {
+        entity.armorSlots.forEach(slot => {
+            if (!slot.empty) {
+                console.log(slot)
+                slot.addTagElement('style', 'heavy')
+            }
+        })
+    }
+
+    if (entity.type === 'minecraft:drowned') {
+        entity.armorSlots.forEach(slot => {
+            if (!slot.empty) {
+                console.log(slot)
+                slot.addTagElement('style', 'samurai')
+            }
+        })
     }
 })
