f494bcc4315417f2d4fc1ab679755527e794b74e
Kevin Murphy <kevin@eorbit.net>
2025-02-21 19:23:53 -0800

    Fix namespace for amphibious effect.

--- kubejs/startup_scripts/amphibious.js
+++ kubejs/startup_scripts/amphibious.js
@@ -1,6 +1,6 @@
 StartupEvents.registry('mob_effect', event => {
-    event.create('amphibious') // Create the effect under "kubejs:amphibious"
-      .color(0x219985) // Sets the color of the Effect's Particles.
-      .beneficial() // Categorizes the Effect as Beneficial.
-  })
-  
+    event
+        .create('spectral_seas:amphibious') // Create the effect under "kubejs:amphibious"
+        .color(0x219985) // Sets the color of the Effect's Particles.
+        .beneficial() // Categorizes the Effect as Beneficial.
+})
