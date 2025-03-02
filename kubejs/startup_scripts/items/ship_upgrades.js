bece43b86c00f997cd8e17f07aa91e4e19464084
Kevin Murphy <kevin@eorbit.net>
2025-03-01 21:42:19 -0800

    Switch to spectral_seas namespace for ship upgrades.

--- kubejs/startup_scripts/items/ship_upgrades.js
+++ kubejs/startup_scripts/items/ship_upgrades.js
@@ -1,12 +1,14 @@
 StartupEvents.registry('item', event => {
-    event.create('ship_speed_upgrade')
+    event
+        .create('spectral_seas:ship_speed_upgrade')
         .displayName('Ship Speed Upgrade')
         .texture('spectral_seas:item/ship_speed_upgrade')
         .maxStackSize(16)
-        .useDuration(itemstack => 20);
-    event.create('ship_cargo_upgrade')
+        .useDuration(itemstack => 20)
+    event
+        .create('spectral_seas:ship_cargo_upgrade')
         .displayName('Ship Cargo Upgrade')
         .texture('spectral_seas:item/ship_storage_upgrade')
         .maxStackSize(16)
-        .useDuration(itemstack => 20);
-});
+        .useDuration(itemstack => 20)
+})
