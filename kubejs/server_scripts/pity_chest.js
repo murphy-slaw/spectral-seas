4ed1030cd6a102a6e7c93a417b154ef93b6a8726
Kevin Murphy <kevin@eorbit.net>
2025-03-04 05:57:04 -0800

    Make pity drops into a template function.

--- kubejs/server_scripts/pity_chest.js
+++ kubejs/server_scripts/pity_chest.js
@@ -1,24 +1,35 @@
-
+//priority: 10000
 /*
     Keep track of how many times a player has opened a chest with the given loot table.
 */
 
-LootJS.modifiers(event => {
-    let lootTable = "nova_structures:chests/undead_crypts_grave"
+function addPityDrops (event, lootTable, chance, lootCallback) {
     event
         .addLootTableModifier(lootTable)
         .playerPredicate(player => {
-            let counts = player.persistentData.getCompound("ChestLootCounts")
-            counts[lootTable] ? counts[lootTable]++ : counts[lootTable] = 1
-            player.persistentData.put("ChestLootCounts",counts)
-            let roll = Utils.getRandom().nextInt(20)
-            console.infof("Roll: %s Score: %s", roll, counts[lootTable])
+            let counts = player.persistentData.getCompound('ChestLootCounts')
+            counts[lootTable] ? counts[lootTable]++ : (counts[lootTable] = 1)
+            player.persistentData.put('ChestLootCounts', counts)
+            let roll = Utils.getRandom().nextInt(chance)
+            console.infof('Roll: %s Score: %s', roll, counts[lootTable])
             return counts[lootTable] > roll
         })
-        .addLoot("artifacts:bunny_hoppers")
+        .addLoot(lootCallback(event))
         .apply(context => {
-            let counts = context.player.persistentData.getCompound("ChestLootCounts")
+            let counts =
+                context.player.persistentData.getCompound('ChestLootCounts')
             counts[lootTable] = 0
-            context.player.persistentData.put("ChestLootCounts",counts)
+            context.player.persistentData.put('ChestLootCounts', counts)
         })
+}
+
+LootJS.modifiers(event => {
+    addPityDrops(
+        event,
+        'nova_structures:chests/undead_crypts_grave',
+        20,
+        () => {
+            return 'artifacts:bunny_hoppers'
+        }
+    )
 })
