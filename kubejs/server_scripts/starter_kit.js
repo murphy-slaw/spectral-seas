1dfeb959844fb7a8603bea8b55e9c285bf9e8e5c
Kevin Murphy <kevin@eorbit.net>
2025-03-10 16:31:58 -0700

    Visual effect tweaks.

--- kubejs/server_scripts/starter_kit.js
+++ kubejs/server_scripts/starter_kit.js
@@ -91,12 +91,13 @@ function getRandomParrotVariant () {
 }
 
 function givePlayerParrot (player) {
-    const parrot = player.block.createEntity('minecraft:parrot')
+    let parrot = player.block.createEntity('minecraft:parrot')
     parrot.copyPosition(player)
     parrot.setVariant(getRandomParrotVariant())
     parrot.setCustomName(Utils.randomOf(Utils.random, names))
     parrot.spawn()
     parrot.tame(player)
+    parrot.setEntityOnShoulder(player)
 }
 
 function givePlayerBundle (player, contents) {
@@ -105,41 +106,43 @@ function givePlayerBundle (player, contents) {
     player.give(bundle)
 }
 PlayerEvents.loggedIn(event => {
-    const player = event.player
+    let player = event.player
     if (!player.stages.has('starter_kit')) {
+        Client.options.setCameraType('third_person_back')
+        if (player.isLocalPlayer()) return
         player.stages.add('starter_kit')
 
-        Client.options.setCameraType('third_person_back')
+        Utils.server.scheduleInTicks(0, () => {})
+        Utils.server.scheduleInTicks(20, () => {
+            givePlayerBundle(player, [
+                Item.of('minecraft:apple', 3),
+                Item.of('minecraft:bread', 3),
+                Item.of('minecraft:torch', 4),
+            ])
 
-        givePlayerBundle(player, [
-            Item.of('minecraft:apple', 3),
-            Item.of('minecraft:bread', 3),
-            Item.of('minecraft:torch', 4),
-        ])
+            givePlayerParrot(player)
 
-        givePlayerParrot(player)
-        Utils.server.scheduleInTicks(40, () => {
-            parrot.setEntityOnShoulder(player)
-        })
-
-        player.give(
-            Item.of(
-                'patchouli:guide_book',
-                '{display:{Name:\'{"translate":"eccentrictome.name","with":[{"color":"green","translate":"The Mariner\\\'s Handbook"}]}\'},"eccentrictome:is_tome":1b,"eccentrictome:mods":{simplyswords:{0:{Count:1b,id:"patchouli:guide_book",tag:{"patchouli:book":"simplyswords:runic_grimoire"}}},spectrum:{0:{Count:1b,id:"spectrum:guidebook"}}},"eccentrictome:version":1,"patchouli:book":"patchouli:mariners_handbook"}'
-            )
-        )
-
-        let weapon = Item.of('simplyswords:iron_cutlass', 1)
-            .enchant('spellbound:storied', 1)
-            .withName("Grandad's Nasty Old Cutlass")
-            .withLore(
-                "You're sure there's a blade somewhere under all that rust."
+            player.give(
+                Item.of(
+                    'patchouli:guide_book',
+                    '{display:{Name:\'{"translate":"eccentrictome.name","with":[{"color":"green","translate":"The Mariner\\\'s Handbook"}]}\'},"eccentrictome:is_tome":1b,"eccentrictome:mods":{simplyswords:{0:{Count:1b,id:"patchouli:guide_book",tag:{"patchouli:book":"simplyswords:runic_grimoire"}}},spectrum:{0:{Count:1b,id:"spectrum:guidebook"}}},"eccentrictome:version":1,"patchouli:book":"patchouli:mariners_handbook"}'
+                )
             )
 
-        weapon.setDamageValue(weapon.maxDamage)
-        player.give(weapon)
-        Utils.server.scheduleInTicks(40, () => {
-            Client.options.setCameraType('first_person')
+            let weapon = Item.of('simplyswords:iron_cutlass', 1)
+                .enchant('spellbound:storied', 1)
+                .withName("Grandad's Nasty Old Cutlass")
+                .withLore(
+                    "You're sure there's a blade somewhere under all that rust."
+                )
+            weapon.setDamageValue(weapon.maxDamage)
+            player.give(weapon)
+            Utils.server.scheduleRepeatingInTicks(1, ctx => {
+                if (player.hasMovementInput()) {
+                    Client.options.setCameraType('first_person')
+                    ctx.clear()
+                }
+            })
         })
     }
 })
