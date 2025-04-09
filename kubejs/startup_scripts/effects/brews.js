883105f0f620aadce6e0c26814ef76e2bc184158
Kevin Murphy <kevin@eorbit.net>
2025-04-08 19:12:10 -0700

    Tweak cauldron effect colors.

--- kubejs/startup_scripts/effects/brews.js
+++ kubejs/startup_scripts/effects/brews.js
@@ -1,6 +1,6 @@
 StartupEvents.registry('mob_effect', event => {
     event.create('spectral_seas:lapis_effect').color(0x20509c).beneficial()
-    event.create('spectral_seas:moss_effect').color(0x008700).beneficial()
-    event.create('spectral_seas:rotten_effect').color(0x870000).beneficial()
-    event.create('spectral_seas:mushroom_effect').color(0xce2d2b).beneficial()
+    event.create('spectral_seas:moss_effect').color(0x495e27).beneficial()
+    event.create('spectral_seas:rotten_effect').color(0x6f4d1b).beneficial()
+    event.create('spectral_seas:mushroom_effect').color(0xbf7272).beneficial()
 })
