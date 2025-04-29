d63f2569e37d4f2459da985b0e9fdba4511b738d
Kevin Murphy <kevin@eorbit.net>
2025-04-28 18:00:36 -0700

    Replace all chiseled bookshelves in structures with bookshelves.

---
+++ kubejs/server_scripts/remove_enchanting_blocks.js
@@ -0,0 +1,11 @@
+MoreJSEvents.structureLoad(event => {
+    event.forEachPalettes(palette => {
+        palette.forEach(blockInfo => {
+            if (blockInfo.block === Blocks.ENCHANTING_TABLE) {
+                blockInfo.setBlock('cartography_table')
+            } else if (blockInfo.block === Blocks.CHISELED_BOOKSHELF) {
+                blockInfo.setBlock('bookshelf')
+            }
+        })
+    })
+})
