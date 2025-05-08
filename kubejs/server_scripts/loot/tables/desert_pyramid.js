a431845a9102e6d799002f421b8a56570636ec0e
Kevin Murphy <kevin@eorbit.net>
2025-05-08 10:33:35 -0700

    More desert structure tweaks

--- kubejs/server_scripts/loot/tables/desert_pyramid.js
+++ kubejs/server_scripts/loot/tables/desert_pyramid.js
@@ -3,31 +3,45 @@ const pharaoh_regalia = ctx => {
     console.log('pharaoh')
     return [
         Loot.randomEnchantedFrom(CHAINMAIL_ARMOR, CITRINE_ENCHANTS).damage([0, 1]),
+
         Loot.randomEnchantedFrom(DIAMOND_ARMOR, CITRINE_ENCHANTS).damage(0),
+
         LootEntry.of('minecraft:blue_dye')
             .when(c => c.randomChance(0.7))
             .limitCount([2, 5]),
+
         LootEntry.of('minecraft:purple_dye').when(c => c.randomChance(0.7)),
+
         LootEntry.of('minecraft:honey_bottle')
             .when(c => c.randomChance(0.7))
             .limitCount([2, 5]),
+
         LootEntry.of('minecraft:yellow_banner').when(c => c.randomChance(0.7)),
+
         LootEntry.of('minecraft:purple_banner').when(c => c.randomChance(0.7)),
+
         LootEntry.of('minecraft:blue_banner').when(c => c.randomChance(0.7)),
+
         LootEntry.of('supplementaries:urn', {
             Items: [{ Slot: 0, Count: 5, id: 'minecraft:bone' }],
         })
             .when(c => c.randomChance(0.7))
             .limitCount([1, 3]),
+
         LootEntry.of('artifacts:antidote_vessel').when(c => c.randomChance(0.7)),
+
         LootEntry.of('minecraft:gold_ingot')
             .when(c => c.randomChance(0.7))
             .limitCount([1, 6]),
+
         LootEntry.of('minecraft:gold_block').when(c => c.randomChance(0.7)),
+
         LootEntry.of('minecraft:golden_carrot')
             .when(c => c.randomChance(0.7))
             .limitCount([1, 5]),
+
         LootEntry.of('minecraft:golden_apple').when(c => c.randomChance(0.7)),
+
         LootEntry.of('minecraft:enchanted_golden_apple').when(c => c.randomChance(0.05)),
     ]
 }
@@ -42,20 +56,26 @@ const tomb_guardian_remains = ctx => {
         )
             .damage([0.25, 0.5])
             .when(c => c.randomChance(0.7)),
+
         Loot.enchantedFrom('minecraft:shield', CITRINE_ENCHANTS)
             .damage([0.5, 0.75])
             .when(c => c.randomChance(0.5)),
+
         Loot.enchantedFrom('minecraft:golden_helmet', CITRINE_ENCHANTS)
             .addNbt({
                 Trim: { material: Loot.randomOf(TRIM_MATERIAL), pattern: 'minecraft:dune' },
             })
             .when(c => c.randomChance(0.5))
             .damage([0, 0.5]),
+
         Loot.randomEnchantedFrom(LEATHER_ARMOR, CITRINE_ENCHANTS)
             .damage([0, 0.5])
             .when(c => c.randomChance(0.7)),
+
         LootEntry.of('bones').limitCount([3, 7]),
+
         LootEntry.of('rotten_flesh').limitCount([3, 7]),
+
         LootEntry.of('artifacts:crystal_heart').when(c => c.randomChance(0.1)),
     ]
 }
@@ -67,26 +87,35 @@ const court_librarian_cache = ctx => {
         LootEntry.of('spectrum:gilded_book')
             .when(c => c.randomChance(0.7))
             .limitCount([1, 2]),
+
         LootEntry.of('minecraft:paper')
             .when(c => c.randomChance(0.7))
             .limitCount([2, 6]),
+
         LootEntry.of('minecraft:writable_book').when(c => c.randomChance(0.7)),
+
         LootEntry.of('minecraft:book')
             .when(c => c.randomChance(0.7))
             .limitCount([1, 3]),
+
         LootEntry.of('minecraft:ink_sac')
             .when(c => c.randomChance(0.7))
             .limitCount([1, 3]),
+
         LootEntry.of('minecraft:feather')
             .when(c => c.randomChance(0.7))
             .limitCount([2, 6]),
+
         LootEntry.of('minecraft:map')
             .when(c => c.randomChance(0.7))
             .limitCount([1, 2]),
+
         LootEntry.of('minecraft:gold_ingot')
             .when(c => c.randomChance(0.7))
             .limitCount([1, 3]),
+
         LootEntry.of('minecraft:brush').when(c => c.randomChance(0.7)),
+
         LootEntry.of('artifacts:obsidian_skull').when(c => c.randomChance(0.1)),
     ]
 }
@@ -98,28 +127,36 @@ const caravan_driver_kit = ctx => {
         LootEntry.of('minecraft:cooked_rabbit')
             .when(c => c.randomChance(0.7))
             .limitCount([2, 5]),
+
         LootEntry.of('minecraft:bread')
             .when(c => c.randomChance(0.7))
             .limitCount([3, 7]),
+
         Loot.potionOf('water')
             .when(c => c.randomChance(0.7))
             .limitCount([3, 8]),
+
         LootEntry.of('minecraft:saddle').when(c => c.randomChance(0.7)),
+
         Loot.randomEntryOf([
             'minecraft:leather_horse_armor',
             'minecraft:iron_horse_armor',
             'minecraft:golden_horse_armor',
         ]),
+
         LootEntry.of('minecraft:spyglass').when(c => c.randomChance(0.7)),
+
         LootEntry.of('leather')
             .when(c => c.randomChance(0.7))
             .limitCount([1, 4]),
+
         Loot.randomEnchantedFrom(
             ['minecraft:leather_boots', 'minecraft:leather_leggings'],
             ['minecraft:fire_protection']
         )
             .damage([0.5, 0.75])
             .when(c => c.randomChance(0.7)),
+
         Loot.randomEntryOf(['minecraft:iron_pickaxe', 'minecraft:iron_shovel']).limitCount([1, 2]),
     ]
 }
@@ -128,19 +165,25 @@ const caravan_driver_kit = ctx => {
 const tomb_treasury = ctx => {
     return [
         LootEntry.of('minecraft:iron_block').when(c => c.randomChance(0.7)),
+
         LootEntry.of('minecraft:gold_block')
             .when(c => c.randomChance(0.7))
             .limitCount([1, 3]),
+
         LootEntry.of('minecraft:anvil').when(c => c.randomChance(0.2)),
+
         LootEntry.of('minecraft:iron_ingot')
             .when(c => c.randomChance(0.7))
             .limitCount([2, 4]),
+
         LootEntry.of('minecraft:gold_ingot')
             .when(c => c.randomChance(0.7))
             .limitCount([4, 8]),
+
         LootEntry.of('minecraft:dune_armor_trim_smithing_template')
             .when(c => c.randomChance(0.7))
             .limitCount([1, 2]),
+
         Loot.randomEntryOf([
             'minecraft:chiseled_sandstone',
             'minecraft:smooth_sandstone',
@@ -149,6 +192,7 @@ const tomb_treasury = ctx => {
         ])
             .when(c => c.randomChance(0.7))
             .limitCount([8, 24]),
+
         Loot.randomEntryOf([
             'minecraft:chiseled_red_sandstone',
             'minecraft:smooth_red_sandstone',
@@ -157,6 +201,7 @@ const tomb_treasury = ctx => {
         ])
             .when(c => c.randomChance(0.7))
             .limitCount([8, 24]),
+
         LootEntry.of('minecraft:red_sand')
             .when(c => c.randomChance(0.7))
             .limitCount([16, 32]),
@@ -168,15 +213,16 @@ LootJS.modifiers(event => {
     Loot.smartReplacePools(event, 'minecraft:chests/desert_pyramid', pool)
     Loot.smartReplacePools(event, 'mostructures:pyramid_custom', pool)
 
-    const grave_pool = [tomb_guardian_remains, court_librarian_cache, caravan_driver_kit]
-    Loot.smartReplacePools(
-        event,
-        'nova_structures:chests/desert_ruins/desert_ruin_grave',
-        grave_pool
-    )
+    Loot.smartReplacePools(event, 'nova_structures:chests/desert_ruins/desert_ruin_grave', [
+        tomb_guardian_remains,
+        court_librarian_cache,
+        caravan_driver_kit,
+    ])
+
     Loot.smartReplacePools(event, 'nova_structures:chests/desert_ruins/desert_ruin_main_temple', [
         pharaoh_regalia,
     ])
+
     Loot.smartReplacePools(
         event,
         'nova_structures:chests/desert_ruins/desert_ruin_lesser_treasure',
@@ -184,14 +230,13 @@ LootJS.modifiers(event => {
     )
 
     event
-        .addLootTableModifier(
-            'nova_structures:chests/desert_ruins/desert_ruin_house',
-            '/nova_structures:archeology/desert_ruin.*/'
-        )
+        .addLootTypeModifier(LootType.CHEST)
         .replaceLoot('minecraft:emerald', 'minecraft:gold_nugget', true)
+
     event
         .addLootTableModifier('archeologyplus:chests/desert_temple_food')
         .matchLoot('minecraft:suspicious_stew')
         .removeLoot('minecraft:suspicious_stew')
         .addWeightedLoot(CHOWDER_EFFECTS.map(effect => Loot.chowderOf(effect)))
+        .limitCount([1, 3])
 })
