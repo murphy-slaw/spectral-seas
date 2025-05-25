8fb9e36c91bfc213af1fd7bce97b58aaac53cca7
Kevin Murphy <kevin@eorbit.net>
2025-05-25 14:08:26 -0700

    Recipe for Fisherman's Resin

---
+++ kubejs/server_scripts/recipe/fisherman's resin.js
@@ -0,0 +1,6 @@
+ServerEvents.recipes(event => {
+    event.shaped(Item.of('spectral_seas:fishermans_resin'), [' C ', 'CNC', ' C '], {
+        C: '#coral_fans',
+        N: 'nautilus_shell',
+    })
+})
