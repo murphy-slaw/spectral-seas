55e0f88d6907352fb85f31b2e79117464ad5ef97
Kevin Murphy <kevin@eorbit.net>
2025-12-12 22:48:39 -0800

    Remove detritus from old cauldron mixing system

--- kubejs/startup_scripts/effects/brews.js
+++
@@ -1,6 +0,0 @@
-StartupEvents.registry('mob_effect', (event) => {
-    event.create('spectral_seas:lapis_effect').color(0x20509c).beneficial()
-    event.create('spectral_seas:moss_effect').color(0x495e27).beneficial()
-    event.create('spectral_seas:rotten_effect').color(0x6f4d1b).beneficial()
-    event.create('spectral_seas:mushroom_effect').color(0xbf7272).beneficial()
-})
