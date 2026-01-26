151554e66e304e5b07c4a77e5e4b0ddb05ed69d4
Kevin Murphy <kevin@eorbit.net>
2026-01-25 17:34:08 -0800

    Change seashell tool recipes in json since Kube can't hack it

--- kubejs/server_scripts/recipe/seashell_tools.js
+++
@@ -1,7 +0,0 @@
-ServerEvents.recipes((event) => {
-    event.replaceInput(
-        { output: '#hybrid-aquatic:seashell_tools' },
-        'minecraft:nautilus_shell',
-        '#spectral_seas:shells'
-    )
-})
