7c1c631369a45592b17d2517a05c485339021526
Kevin Murphy <kevin@eorbit.net>
2026-01-30 15:36:15 -0800

    More log spam cleanup

--- kubejs/client_scripts/antique_atlas.js
+++ kubejs/client_scripts/antique_atlas.js
@@ -45,7 +45,7 @@ function getColor(colorName) {
  * @param {string} color
  */
 function addMarker(level, id, pos, label, color) {
-    console.log(id)
+    console.debug(id)
 
     const dyeColor = getColor(color)
 
@@ -76,23 +76,23 @@ function addMarker(level, id, pos, label, color) {
  * @param {ResourceLocation} id
  */
 function deleteMarker(level, id) {
-    console.log(`trying to delete ${id}`)
+    console.debug(`trying to delete ${id}`)
     $WorldSummary.of(level).landmarks().remove(level, $SurveyorClient.getClientUuid(), id)
 }
 
 NetworkEvents.dataReceived('AddMarker', (event) => {
     let marker = event.data
-    console.log(marker)
+    console.debug(marker)
     addMarker(Client.level, marker.location, marker.pos, marker.label, marker.color)
 })
 
 NetworkEvents.dataReceived('DeleteMarker', (event) => {
-    console.log('recieved DeleteMarker')
+    console.debug('recieved DeleteMarker')
     deleteMarker(Client.level, event.data.location)
 })
 
 NetworkEvents.dataReceived('DeleteGrave', (event) => {
-    console.log('recieved DeleteGrave')
+    console.debug('recieved DeleteGrave')
     const targetPos = BlockPos(event.data.pos.x, event.data.pos.y, event.data.pos.z)
     const map = $WorldSummary
         .of(event.level)
@@ -100,7 +100,7 @@ NetworkEvents.dataReceived('DeleteGrave', (event) => {
         .asMap($SurveyorClient.getClientUuid(), null)
     let targetLandmark
     map.forEach((k, v) => {
-        console.log(k.getPath())
+        console.debug(k.getPath())
         if (k.getPath().startsWith('grave')) {
             const pos = v.get($LandmarkComponentTypes.POS)
             if (pos.equals(targetPos)) {
