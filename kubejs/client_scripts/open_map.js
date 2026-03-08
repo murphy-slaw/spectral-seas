647c1f96a21db2763510509780c3a7606443bce1
Kevin Murphy <kevin@eorbit.net>
2026-03-07 16:36:22 -0800

    Try to smart zoom the atlas when opening an explorer map

--- kubejs/client_scripts/open_map.js
+++ kubejs/client_scripts/open_map.js
@@ -1,36 +1,42 @@
-const AtlasScreen = Java.loadClass('folk.sisby.antique_atlas.gui.AtlasScreen')
-const Integer = Java.loadClass('java.lang.Integer')
-const ColumnPos = Java.loadClass('net.minecraft.server.level.ColumnPos')
-const Klass = Java.class.forName('folk.sisby.antique_atlas.gui.AtlasScreen')
+const $AtlasScreen = Java.loadClass('folk.sisby.antique_atlas.gui.AtlasScreen')
+const $AntiqueAtlas = Java.loadClass('folk.sisby.antique_atlas.AntiqueAtlas')
+const $ColumnPos = Java.loadClass('net.minecraft.server.level.ColumnPos')
 
-var setTargetPosition
-var zoomOut
-// Promise not to tell Lat
-Klass.getDeclaredMethods().forEach((meth) => {
-    if (meth.getName() === 'setTargetPosition') {
-        meth.setAccessible(true)
-        setTargetPosition = meth
-        return
-    }
-    if (meth.getName() === 'zoomOut') {
-        meth.setAccessible(true)
-        zoomOut = meth
-        return
-    }
-})
+const MAX_ZOOM = 1 << $AntiqueAtlas.CONFIG.maxTileChunks
 
 /* 
 The server sends an OpenMap packet when an explorer map enters the player's inventory 
 for the first time
  */
 NetworkEvents.dataReceived('OpenMap', (event) => {
-    const pos = event.data
-    const screen = new AtlasScreen()
+    /** @type {Internal.ColumnPos} */
+    const columnPos = $ColumnPos(event.data.x, event.data.z)
+
+    const screen = new $AtlasScreen()
     screen.prepareToOpen()
     //This deselects the player button so that the screen doesn't snap back to the player immediately
     screen.clearTargetBookmarks(null)
-    while (zoomOut.invoke(screen, true, Integer.valueOf('32'))) {}
     Client.setScreen(screen)
-    //Move the center of the map to the position we got in the packet
-    setTargetPosition.invoke(screen, ColumnPos(pos.x, pos.z))
+
+    // Set the center of the map to the position we got in the packet. Use setMapPosition instead of setTargetPosition to skip the animation
+    screen.setMapPosition(columnPos.x(), columnPos.z())
+
+    while (screen.zoomIn(false, 0)) {}
+    let zoom = 1
+    const bounds = screen.getBounds()
+
+    // Try to fit both the target and the player marker on screen
+    while (true) {
+        let targetScreenX = screen.worldXToScreenX(columnPos.x())
+        let targetScreenY = screen.worldZToScreenY(columnPos.z())
+        let playerScreenX = screen.worldXToScreenX(event.player.x)
+        let playerScreenY = screen.worldZToScreenY(event.player.z)
+        let xDist = Math.abs(targetScreenX - playerScreenX)
+        let yDist = Math.abs(targetScreenY - playerScreenY)
+        if (xDist < bounds.width / 2 && yDist < bounds.height / 2) break
+
+        zoom = zoom << 1
+        if (zoom > MAX_ZOOM) break
+        screen.zoomOut(true, zoom)
+    }
 })
