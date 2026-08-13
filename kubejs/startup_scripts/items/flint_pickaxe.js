e1e2a198d62c6400c42ad66844aabc6623298887
Kevin Murphy <kevin@eorbit.net>
2026-08-13 11:02:31 -0700

    Add terrible flint pickaxes
    as a mercy to to the truly unfortunate

---
+++ kubejs/startup_scripts/items/flint_pickaxe.js
@@ -0,0 +1,3 @@
+StartupEvents.registry('minecraft:item', (event) => {
+    event.create('spectral_seas:flint_pickaxe', 'pickaxe').tier('wood').maxDamage(20).tag("minecraft:pickaxes").tag('c:tools')
+})
